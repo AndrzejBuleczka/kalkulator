c075187b38936ea60e5c908664cdc9bf2641e4e0
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2022-09-30 20:53:46 +0200

    initial coding for exercises

---
+++ kalkulator.js
@@ -0,0 +1,30 @@
+var operationsHistory = []
+
+do {
+  var choice = prompt('Czy chcesz obliczać dalej? Wpisz N aby zakńczyć działanie.')
+
+  if(choice !== 'N') {
+
+    var val1 = prompt('Podaj pierwszą liczbę');
+    var val2 = prompt('Podaj drugą liczbę');
+    var operation = prompt('Podaj działanie (+-*/).');
+
+    if(operation === '+') {
+      console.log(val1 + ' + ' + val2 + ' = ' + (parseInt(val1) + parseInt(val2)));
+      operationsHistory.push(val1 + ' + ' + val2 + ' = ' + (parseInt(val1) + parseInt(val2)))
+    }
+    if(operation === '-') {
+      console.log(val1 + ' - ' + val2 + ' = ' + (parseInt(val1) - parseInt(val2)));
+      operationsHistory.push(val1 + ' - ' + val2 + ' = ' + (parseInt(val1) - parseInt(val2)))
+    }
+    if(operation === '*') {
+      console.log(val1 + ' * ' + val2 + ' = ' + (parseInt(val1) * parseInt(val2)));
+      operationsHistory.push(val1 + ' * ' + val2 + ' = ' + (parseInt(val1) * parseInt(val2)))
+    }
+    if(operation === '/') {
+      console.log(val1 + ' / ' + val2 + ' = ' + (parseInt(val1) / parseInt(val2)));
+      operationsHistory.push(val1 + ' / ' + val2 + ' = ' + (parseInt(val1) / parseInt(val2)))
+    }
+  }
+} while(choice !== "N");
+console.log(operationsHistory);
