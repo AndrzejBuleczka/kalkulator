5403cced906fdf3f9c5e8ad0d50f294fb2648806
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2022-10-01 14:11:03 +0200

    first version

--- kalkulator.js
+++ kalkulator.js
@@ -1,36 +1,37 @@
 var operationsHistory = []
 
 do {
-  var choice = prompt('Czy chcesz obliczać dalej? Wpisz N aby zakńczyć działanie.')
 
-  if(choice !== 'N') {
+  var val1 = parseInt(prompt('Podaj pierwszą liczbę'));
+  var val2 = parseInt(prompt('Podaj drugą liczbę'));
+  var operation = prompt('Podaj działanie (+-*/).');
+  var result;
 
-    var val1 = parseInt(prompt('Podaj pierwszą liczbę'));
-    var val2 = parseInt(prompt('Podaj drugą liczbę'));
-    var operation = prompt('Podaj działanie (+-*/).');
-    var result;
-
-    if (isNaN(val1) || isNaN(val2)) {
-      result = "Niepoprawne wartości"
-    } else {
-      if(operation === '+') {
-        result = val1 + ' + ' + val2 + ' = ' + (val1 + val2)
-      }
-       else if(operation === '-') {
-        result = val1 + ' - ' + val2 + ' = ' + (val1 - val2)
-      }
-      else if(operation === '*') {
-        result = val1 + ' * ' + val2 + ' = ' + (val1 * val2)
-      }
-      else if(operation === '/') {
-        result = val1 + ' / ' + val2 + ' = ' + (val1 / val2)
+  if (isNaN(val1) || isNaN(val2)) {
+    result = "Niepoprawne wartości"
+  } else {
+    if(operation === '+') {
+      result = val1 + ' + ' + val2 + ' = ' + (val1 + val2)
+    }
+    else if(operation === '-') {
+      result = val1 + ' - ' + val2 + ' = ' + (val1 - val2)
+    }
+    else if(operation === '*') {
+      result = val1 + ' * ' + val2 + ' = ' + (val1 * val2)
+    }
+    else if(operation === '/') {
+      if(val2 === 0) {
+        result = "Nie możesz dzielić przez 0!!!"
       } else {
-        result = "Niepoprawna operacja"
+        result = val1 + ' / ' + val2 + ' = ' + (val1 / val2)
       }
+    } else {
+      result = "Niepoprawna operacja"
+    }
     }
 
-    console.log(result);
-    operationsHistory.push(result)
-  }
+  console.log(result);
+  operationsHistory.push(result)
+  var choice = prompt('Czy chcesz obliczać dalej? Wpisz N aby zakńczyć działanie.')
 } while(choice !== "N");
 console.log(operationsHistory);
